d4110f00d0453657bf6b59f03d2a1d59f740679c
YanX <typhoon322@qq.com>
2018-04-24 18:02:41 +0800

    beta0.0.1

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const AsyncUtil = require('./lib/asyncUtil');
+var asyncUtil = new AsyncUtil();
 
 const Koa = require('koa');
-
-const router = require('koa-router')();
-
 const app = new Koa();
 
-const cors = require('koa-cors');
+const Router = require('koa-router');
+var router = new Router();
 
+const BodyParser = require('koa-bodyparser');
+app.use(BodyParser());
+
+const cors = require('koa-cors');
 app.use(cors());
 
 app.use(async(ctx,next) => {
@@ -15,34 +18,83 @@ app.use(async(ctx,next) => {
     await next();
 });
 
-/**
- * 获取上传token
- * name：bucket 的名字
- */
-router.get('/auth/:name', async (ctx, next) => {
-    var name = ctx.params.name;
-    ctx.response.body = {"token": new AsyncUtil().fetchUploadToken(name)};
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/pandashit');
+const Photo = mongoose.model('Photo', {
+    url: String,
+    title: String,
+    desc: String,
+    createdTime: Date
 });
 
-var qiniu = require('qiniu');
+router.get('/', async (ctx, next) => {
+    ctx.response.body = {code: 0, data: {msg: 'index'}, msg: ''};
+});
 
-function getMac(){
-    var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
-    console.log('mac : '+mac);
-    return mac;
-}
+router.get('/index', async (ctx, next) => {
+    await Photo.find({}, function (err, results) {
+        if(err){
+            console.error(err);
+            ctx.response.body = {code: 1, msg: 'Internal Error'};
+          return;
+        }
+      }).then(function (res) {
+        console.log(res);
+          if(res){
+            ctx.response.body = {code: 0, data: res, msg: 'Success'};//以json数据类型返回值
+          }
+      });
+});
+
+/**
+ * 获取上传token
+ */
+router.get('/auth', async (ctx, next) => {
+    ctx.response.body = {"token": asyncUtil.fetchUploadToken()};
+});
 
 
-function getBucketManager(){
-    var mac = getMac();
-    var config = new qiniu.conf.Config();
-    config.zone = qiniu.zone.Zone_z2;
-    var bucketManager = new qiniu.rs.BucketManager(mac,config);
-    return bucketManager;
-}
+router.post('/add', async(ctx) => {
+    let body = ctx.request.body;
+    console.info(body);
+    const array = body.items;
+    array.forEach(element => {
+        element.createdTime = new Date();
+    });
 
-app.use(router.routes());
+    await Photo.create(array, function (err, candies) {
+        if(err){
+            console.error(err);
+            ctx.response.body = {code: 1, msg: 'Internal error'};
+            return;
+        }
+    }).then(function (res) {
+        console.log('==========添加成功=========');
+        console.log(res);
+        ctx.response.body = {code: 0, msg: 'success'};
+    });
+});
 
-app.listen(3001,function(){
-    console.log('CORS-enabled web server listening on port 3001');
+// var qiniu = require('qiniu');
+
+// function getMac(){
+//     var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+//     console.log('mac : '+mac);
+//     return mac;
+// }
+
+
+// function getBucketManager(){
+//     var mac = getMac();
+//     var config = new qiniu.conf.Config();
+//     config.zone = qiniu.zone.Zone_z2;
+//     var bucketManager = new qiniu.rs.BucketManager(mac,config);
+//     return bucketManager;
+// }
+
+
+app.use(router.routes()).use(router.allowedMethods);
+
+app.listen(3020,function(){
+    console.log('CORS-enabled web server listening on port 3020');
 });
